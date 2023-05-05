13959a3939ec6d6c1dacd1b6f7362fc6eccd01bc
Aman <amanbeer899@gmail.com>
2023-05-05 16:40:23 -0700

    added neo4j connection and search intents

--- actions/actions.py
+++ actions/actions.py
@@ -1,39 +1,37 @@
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from neo4j import GraphDatabase
 
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class FindFileAction(Action):
+    def name(self) -> Text:
+        return "action_find_file"
 
-# given a graphdatabase driver, return a list of all the filenamnes in the database
-def get_filenames(driver):
-    session = driver.session()
-    result = session.run("MATCH (n:File) RETURN n.name AS name")
-    filenames = []
-    for record in result:
-        filenames.append(record["name"])
-    session.close()
-    return filenames
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[Dict[Text, Any]]:
 
-class rasa_test_work(Action): 
-    def name(self): 
-        return "action_rasa_test_work"
+        # Get the file name and folder name from the slots
+        file_name = tracker.get_slot("file_name")
+        folder_name = tracker.get_slot("folder_name")
 
-    def run(self, dispatcher, tracker, domain):
-        from neo4j import GraphDatabase
-        driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"))
-        filenames = get_filenames(driver)
-        dispatcher.utter_message(text="The filenames are: " + str(filenames))
+        # Connect to the Neo4j database
+        driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+        with driver.session() as session:
+            # Find the folder node with the given name
+            result = session.run("MATCH (folder:Folder {name: $name}) RETURN folder", name=folder_name)
+            folder_node = result.single().get("folder")
+
+            # Find the file node with the given name inside the folder node
+            result = session.run("MATCH (folder)-[:CONTAINS]->(file:File {name: $name}) RETURN file", name=file_name, folder=folder_node)
+            file_node = result.single().get("file")
+
+            if file_node:
+                # If the file node is found, extract the file path and send it as a message
+                file_path = file_node.get("path")
+                dispatcher.utter_message(f"The file {file_name} is located at {file_path}")
+            else:
+                # If the file node is not found, send a message indicating that the file was not found
+                dispatcher.utter_message(f"Sorry, I couldn't find the file {file_name} in the folder {folder_name}")
+
         return []
-    
