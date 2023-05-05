7a418a8520ca4119322652a1ce600acb9b098b39
Aman <amanbeer899@gmail.com>
2023-05-04 18:23:39 -0700

    updated basic api script for neo4j

--- actions/actions.py
+++ actions/actions.py
@@ -1,18 +1,8 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
 
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -25,3 +15,25 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+# given a graphdatabase driver, return a list of all the filenamnes in the database
+def get_filenames(driver):
+    session = driver.session()
+    result = session.run("MATCH (n:File) RETURN n.name AS name")
+    filenames = []
+    for record in result:
+        filenames.append(record["name"])
+    session.close()
+    return filenames
+
+class rasa_test_work(Action): 
+    def name(self): 
+        return "action_rasa_test_work"
+
+    def run(self, dispatcher, tracker, domain):
+        from neo4j import GraphDatabase
+        driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "1234"))
+        filenames = get_filenames(driver)
+        dispatcher.utter_message(text="The filenames are: " + str(filenames))
+        return []
+    
